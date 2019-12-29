007b3ffcb2ce15db0098ed89960984e34baea3e4
kurelaitisk <59114902+kurelaitisk@users.noreply.github.com>
2019-12-29 23:25:09 +0200

    Update GameBoard.py

--- GameBoard.py
+++ GameBoard.py
@@ -4,13 +4,12 @@ import Coordinates
 class GameBoard:
     def __init__(self, size):
         self.size = size
-        self.board = GameBoard.GenerateBoard(size)
+        self.board = self.generate_board()
 
-    @staticmethod
-    def GenerateBoard(size):
-        return [[0 for x in range(size)] for y in range(size)]
+    def generate_board(self):
+        return [[0 for x in range(self.size)] for y in range(self.size)]
 
-    def IsValidShipPosition(self, ship_coordinates):
+    def is_valid_ship_position(self, ship_coordinates):
         is_valid = True
         for i in ship_coordinates:
             if self.board[i.get("row")][i.get("column")] != 0:
@@ -18,30 +17,27 @@ class GameBoard:
                 break
         return is_valid
 
-    def AddShip(self, ship_coordinates, ship_length):
+    def add_ship(self, ship_coordinates, ship_length):
         for i in ship_coordinates:
             self.board[i.get("row")][i.get("column")] = ship_length
-        # raise ValueError('You already have ship in this position')
 
-    def AddComputerShips(self, ships):
-
+    def add_ships_randomly(self, ships):
         for i in ships:
             ship_added = False
             while not ship_added:
                 ship_coordinates = Coordinates.Coordinates()
-                ship_coordinates.GenerateRandomCoordinates(self.size - 1, ships[i])
-                if self.IsValidShipPosition(ship_coordinates.coordinates_list):
-                    self.AddShip(ship_coordinates.coordinates_list, ships[i])
+                ship_coordinates.generate_random_coordinates(self.size - 1, ships[i])
+                if self.is_valid_ship_position(ship_coordinates.coordinates_list):
+                    self.add_ship(ship_coordinates.coordinates_list, ships[i])
                     ship_added = True
                 else:
                     ship_added = False
 
-    def AddUserShips(self, ships):
-
+    def add_ships_manually(self, ships):
         print("Do you want to add ships (m)anually or (a)utomatically?")
         ship_generation = input()
         if ship_generation.lower() == "a":
-            self.AddComputerShips(ships)
+            self.add_ships_randomly(ships)
         elif ship_generation.lower() == "m":
             for ship in ships:
                 ship_added = False
@@ -49,37 +45,35 @@ class GameBoard:
                     try:
                         user_ship_coordinates = Coordinates.Coordinates()
                         user_ship_coordinates.length = ships[ship]
-                        user_ship_coordinates.setRowAndColumn(
+                        user_ship_coordinates.set_row_and_column(
                             row=input(
-                                f"Please write X coordinate for ship {ship}. It's size is: {ships[ship]}. Max coordinate {self.size - 1} \r\n"
+                                f"Please write X coordinate for ship {ship}. It's size is: {ships[ship]}."
+                                f" Max coordinate {self.size - 1} \r\n"
                             ),
                             column=input(
-                                f"Please write Y coordinate for ship {ship}. It's size is: {ships[ship]}. Max coordinate {self.size - 1} \r\n"
+                                f"Please write Y coordinate for ship {ship}. It's size is: {ships[ship]}."
+                                f" Max coordinate {self.size - 1} \r\n"
                             ),
                             max_len=self.size,
                         )
-                        user_ship_coordinates.setDirection(
+                        user_ship_coordinates.set_direction(
                             input(
                                 f"Do you want to place ship (H)orizontally or (V)ertically? \r\n"
                             )
                         )
-                        user_ship_coordinates.SetUserCoords(self.size)
-                        if self.IsValidShipPosition(
-                            user_ship_coordinates.coordinates_list
-                        ):
-                            self.AddShip(
-                                user_ship_coordinates.coordinates_list, ships[ship]
-                            )
+                        user_ship_coordinates.set_coordinates_manually(self.size)
+                        if self.is_valid_ship_position(user_ship_coordinates.coordinates_list):
+                            self.add_ship(user_ship_coordinates.coordinates_list, ships[ship])
                             ship_added = True
                         else:
                             raise ValueError("There already is ship in this position")
                     except ValueError as error:
                         print(error)
         else:
-            self.AddUserShips(ships)
-        print("All ships added")
+            self.add_ships_manually(ships)
+        print("All ships added successfully")
 
-    def PrintBoard(self, show_ships=True):
+    def print_board(self, show_ships=True):
         if show_ships:
             for i in self.board:
                 print(i)
@@ -90,7 +84,7 @@ class GameBoard:
             "----------------------------------------------------------------------------"
         )
 
-    def Shoot(self, coordinates, who_shot):
+    def shoot(self, coordinates, who_shot):
         if who_shot == "computer":
             print(
                 f"Computer shoots at: row {[coordinates.row]} and column {[coordinates.column]}"
@@ -102,31 +96,31 @@ class GameBoard:
             self.board[coordinates.row][coordinates.column] = -1
             print("Miss!")
 
-    def ValidateShot(self, coordinates):
-        if not coordinates.IsValidRowAndColumn(self.size):
+    def validate_shot(self, coordinates):
+        if not coordinates.is_valid_row_and_column(self.size):
             raise ValueError("Invalid shot. Shot coordinates not in range")
         elif self.board[coordinates.row][coordinates.column] < 0:
             raise ValueError("Invalid shot. You already shot there")
 
-    def HasLost(self):
-        has_won = True
+    def has_lost(self):
+        has_lost = True
         for i in self.board:
             for j in i:
                 if j > 0:
-                    has_won = False
+                    has_lost = False
                     break
-        return has_won
+        return has_lost
 
-    def UserShot(self, auto_play):
+    def manual_shot(self, auto_play):
         if auto_play.lower() == "y":
-            self.ComputerShot("user")
+            self.random_shot("user")
         else:
             has_shot = False
             while not has_shot:
                 print("Write coordinates to shoot")
-                user_shot_coordinates = Coordinates.Coordinates()
+                shot_coordinates = Coordinates.Coordinates()
                 try:
-                    user_shot_coordinates.setRowAndColumn(
+                    shot_coordinates.set_row_and_column(
                         row=input(
                             f"Please write X coordinate to shoot . Max coordinate {self.size - 1} \r\n"
                         ),
@@ -135,24 +129,24 @@ class GameBoard:
                         ),
                         max_len=self.size,
                     )
-                    self.ValidateShot(user_shot_coordinates)
-                    self.Shoot(user_shot_coordinates, "user")
+                    self.validate_shot(shot_coordinates)
+                    self.shoot(shot_coordinates, "user")
                     has_shot = True
                 except ValueError as error:
                     print(error)
 
-    def ComputerShot(self, who_shot):
+    def random_shot(self, who_shot):
         has_shot = False
         while not has_shot:
             try:
                 computer_shot_coordinates = Coordinates.Coordinates()
-                computer_shot_coordinates.GenerateRandomRowAndColumn(self.size)
-                self.ValidateShot(computer_shot_coordinates)
-                self.Shoot(computer_shot_coordinates, who_shot=who_shot)
+                computer_shot_coordinates.generate_random_row_and_column(self.size)
+                self.validate_shot(computer_shot_coordinates)
+                self.shoot(computer_shot_coordinates, who_shot=who_shot)
                 has_shot = True
             except ValueError as error:
                 has_shot = False
 
     @staticmethod
-    def GetBoardPlan():
+    def get_board_plan():
         return {"Ship shot": -2, "Missed": -1, "Empty": 0}
